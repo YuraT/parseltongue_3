2ac7ba96254eb7e22abea4b3e5540a43bd8f71ea
YuraT <ytatisch@gmail.com>
2019-10-03 18:34:33 -0700

    completed exercise 2

--- 02_capitols.py
+++ 02_capitols.py
@@ -1,14 +1,31 @@
 def main():
     capitols = open("./capitols.txt", "r")
-    array = {}
+    State_to_Capitol = {}
+    Capitol_to_State = {}
 
     for line in capitols:
         temp = []
         temp = line.split(",")
-        array[temp[0]] = temp[1]
-        
-    for i in array:
-        print(array[i])
-    #print(array)
+        State_to_Capitol[temp[0].lower()] = temp[1]
+        Capitol_to_State[temp[1].lower()] = temp[0]
 
+    condition = True
+    while condition:
+        user_input = input("Ready: ").lower()
+
+        if user_input == "done":
+            condition = False
+        else:
+            match_found = False
+            for item in State_to_Capitol:
+                if item == user_input:
+                    print (State_to_Capitol[item])
+                    match_found = True
+            if not(match_found): 
+                for item in Capitol_to_State:
+                    if item == user_input:
+                        print (Capitol_to_State[item])
+            if not(match_found):
+                print("nil")
+    capitols.close()
 main()
